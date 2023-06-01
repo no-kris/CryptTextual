652a51135438dfd34396a8203a3548062d571b36
Kris Treska <ktresk@protonmail.com>
2023-05-31 20:30:01 -0400

    created prototype functions for menu options

--- src/CryptTextual.cpp
+++ src/CryptTextual.cpp
@@ -14,16 +14,31 @@
  *
  *  DISCLAIMER: CryptTextual is a student project created for
  *  educational purposes only. It does not provide financial
- *  advice, and not intended for making trading decisions.
+ *  advice, and is not intended for making trading decisions.
  **************************************************************/
 
 #include <iostream>
 
+struct Account
+{
+    double balance; // Amount left in account
+    double coins;   // Amount of coins owned
+};
+
 // Function prototypes
 void test();
 void banner();
+double getInitialDeposit();
+void initializeAccount(Account *account, double deposit);
+void printMenu();
 int getMenuOption();
-void displayMenuOption(int menuOption);
+void displayMenuOption(int menuOption, Account *account);
+void printHelp();
+void printMarketStats();
+void makeOffer();
+void makeBid();
+void printAccount(Account *account);
+void continueNext();
 
 int main(int argc, char *argv[])
 {
@@ -31,69 +46,166 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// Testing program
+// Test program
 void test()
 {
     banner();
+    Account userAccount;
+    double initialDeposit = getInitialDeposit();
+    initializeAccount(&userAccount, initialDeposit);
     int menuOption = 0;
-    while (menuOption < 1 || menuOption > 6)
+    while (menuOption < 1 || menuOption > 7)
     {
-        menuOption = getMenuOption();
-        displayMenuOption(menuOption);
-    } // EO while (menuOption < 1 || menuOption > 6)
+        while (menuOption != 7)
+        {
+            menuOption = getMenuOption();
+            displayMenuOption(menuOption, &userAccount);
+        } // EO while (menuOption != 7)
+    }     // EO while (menuOption < 1 || menuOption > 6)
 }
 
+// Display banner
 void banner()
 {
-    std::cout << "=============\n"
-              << "CRYPT-TEXTUAL\n"
-              << "=============\n";
+    std::cout << "============================================================\n"
+              << "CRYPTTEXTUAL: Crypto Trading Program\n\n"
+              << "Learn and Practice Crypto Trading.\n\n"
+              << "Hands-on trading simulation  |  Practice trading strategies\n"
+              << "============================================================\n\n";
+}
+
+// Return a value to create an initial account balance
+double getInitialDeposit()
+{
+    double initialDeposit = 0.0;
+    std::cout << "Enter an initial deposit ... ";
+    std::cin >> initialDeposit;
+    return initialDeposit;
+}
+
+// Initialize an account with provided deposit and 0 coins owned
+void initializeAccount(Account *account, double deposit)
+{
+    account->balance = deposit;
+    account->coins = 0.0;
+}
+
+// Display menu options
+void printMenu()
+{
+    std::cout << "\n============== MENU ==============\n"
+              << "| Select an option (1 ... 6)     |\n"
+              << "| Press 1 to print help          |\n"
+              << "| Press 2 to view exchange stats |\n"
+              << "| Press 3 to make an offer       |\n"
+              << "| Press 4 to make a bid          |\n"
+              << "| Press 5 to view your account   |\n"
+              << "| Press 6 to continue            |\n"
+              << "| Press 7 to exit application    |\n"
+              << "==================================\n";
 }
 
 // Return an integer corresponding to menu option
 int getMenuOption()
 {
+    printMenu();
     int menuChoice = 0;
-    std::cout << "=================================\n";
-    std::cout << "| Select an option (1 ... 6)    |\n";
-    std::cout << "| Press 1 to print help         |\n"
-              << "| Press 2 to view exchange stats|\n"
-              << "| Press 3 to make an offer      |\n"
-              << "| Press 4 to make a bid         |\n"
-              << "| Press 5 to view your wallet   |\n"
-              << "| Press 6 to continue           |\n"
-              << "=================================\n";
     std::cout << "Enter choice ... ";
     std::cin >> menuChoice;
     return menuChoice;
 }
 
 // @param menuOption represents a case to perform correct operation
-void displayMenuOption(int menuOption)
+// @param account represents account to perform operation on
+void displayMenuOption(int menuOption, Account *account)
 {
     switch (menuOption)
     {
     case 1:
-        std::cout << "TODO: print help\n";
+        printHelp();
         break;
     case 2:
-        std::cout << "TODO: view exchange stats\n";
+        printMarketStats();
         break;
     case 3:
-        std::cout << "TODO: make an offer\n";
+        makeOffer();
         break;
     case 4:
-        std::cout << "TODO: make a bid\n";
+        makeBid();
         break;
     case 5:
-        std::cout << "TODO: view wallet\n";
+        printAccount(account);
         break;
     case 6:
-        std::cout << "TODO: continue\n";
+        continueNext();
+        break;
+    case 7:
+        std::cout << "Now ending application, bye!\n";
         break;
     default:
         std::cerr << "Not a valid option, try again ... \n";
         // TODO: handleInvalidOption();
         break;
     } // EO switch (menuOption)
+}
+
+// Clarify each menu option
+void printHelp()
+{
+    std::cout << "\n================ HELP ================\n"
+              << "Option 1: Print Help\n"
+              << "   - This option displays the help menu,\n"
+              << "     providing clarifications for each option.\n"
+              << "\n"
+              << "Option 2: View Exchange Stats\n"
+              << "   - This option allows you to view the current\n"
+              << "     statistics and information about the exchange.\n"
+              << "\n"
+              << "Option 3: Make an Offer\n"
+              << "   - With this option, you can make an offer\n"
+              << "     to buy or sell assets on the exchange platform.\n"
+              << "\n"
+              << "Option 4: Make a Bid\n"
+              << "   - Use this option to make a bid on assets\n"
+              << "     available on the exchange.\n"
+              << "\n"
+              << "Option 5: View Your Account\n"
+              << "   - This option displays information about your account,\n"
+              << "     including your balance and coins owned.\n"
+              << "\n"
+              << "Option 6: Continue\n"
+              << "   - Selecting this option allows you to proceed\n"
+              << "     with the current operation.\n"
+              << "\n"
+              << "Option 7: Exit Application\n"
+              << "   - This option terminates the application\n"
+              << "     and exits the program.\n"
+              << "=======================================\n\n";
+}
+
+void printMarketStats()
+{
+    // TODO: include market data
+}
+
+void makeOffer()
+{
+    // TODO: ask user to make offer
+}
+
+void makeBid()
+{
+    // TODO: ask user to make bid
+}
+
+// @param account display account info
+void printAccount(Account *account)
+{
+    std::cout << "Account balance ... $" << account->balance << '\n';
+    std::cout << "Coins owned ... " << account->coins << '\n';
+}
+
+void continueNext()
+{
+    // TODO: continue to next time frame
 }
